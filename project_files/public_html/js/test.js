0c002f0918171e3b3d3322a6b4945b9f47250205
KostasEreksonas <k.ereksonas@gmail.com>
2023-02-22 12:51:52 +0200

    Created pop-up window when clicking on a button

--- project_files/public_html/js/test.js
+++ project_files/public_html/js/test.js
@@ -1,3 +1,28 @@
-function displayInfo() {
-    let element = document.getElementById('more-info');
+let popup = document.getElementById("info-popup");
+let button = document.getElementById("button");
+let close = document.getElementsByClassName("close")[0];
+let closeButton = document.getElementById("closeButton");
+
+// On button click open a pop-up window
+button.onclick = function()
+{
+    popup.style.display = "grid";
+}
+
+// Close the pop-up by clicking either <x> symbol, Close button or outside the pop-up itself
+close.onclick = function()
+{
+    popup.style.display = "none";
+}
+
+closeButton.onclick = function()
+{
+    popup.style.display = "none";
+}
+
+window.onclick = function(event)
+{
+    if (event.target === popup) {
+        popup.style.display = "none";
+    }
 }
